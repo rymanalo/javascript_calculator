f5259dcfa2eaa4bfcc164ec0272cc009057ea74d
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-25 09:29:06 -0700

    Fix errors from Lint.

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,16 @@
 function calculator() {
   var operation = prompt("Pick a mathematical operation to perform");
   var num1 = prompt("Pick a number");
-    num1 = parseInt(num1);
+    num1 = parseInt(num1, 10);
   var num2 = prompt("Pick another number");
-    num2 = parseInt(num2);
+    num2 = parseInt(num2, 10);
   if (operation === "+") {
-    alert(num1 + num2)
+    alert(num1 + num2);
   } else if (operation === "-") {
-    alert(num1 - num2)
+    alert(num1 - num2);
   } else if (operation === "*") {
-    alert(num1 * num2)
+    alert(num1 * num2);
   } else if (operation === "/") {
-    alert(num1 / num2)
+    alert(num1 / num2);
   }
 }
