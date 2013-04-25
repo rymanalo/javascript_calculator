a7b35e4c943c24af38a6f1c9b3aebbf9f3983b52
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-25 09:42:04 -0700

    Change parseInt to parseFloat for precision.

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,9 @@
 function calculator() {
   var operation = prompt("Pick a mathematical operation to perform");
   var num1 = prompt("Pick a number");
-    num1 = parseInt(num1, 10);
+    num1 = parseFloat(num1, 10);
   var num2 = prompt("Pick another number");
-    num2 = parseInt(num2, 10);
+    num2 = parseFloat(num2, 10);
   if (operation === "+") {
     alert(num1 + num2);
   } else if (operation === "-") {
